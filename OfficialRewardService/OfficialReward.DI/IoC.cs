929e4872e28c2d9427f6899a6bdef201cdd4f182
ZhenyaTrif <nomand144@gmail.com>
2019-11-26 17:14:54 +0300

    Main back skeleton

---
+++ OfficialRewardService/OfficialReward.DI/IoC.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfficialReward.DI
+{
+    static public class IoC
+    {
+        static public void IoCCommonRegister(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
---
+++ OfficialRewardService/OfficialReward.Dal/DataAccess/Interfaces/IDataAccess.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfficialReward.Dal.DataAccess.Interfaces
+{
+    public interface IDataAccess
+    {
+    }
+}
